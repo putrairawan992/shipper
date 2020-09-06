e8896be625b0ef6bc46a12e5d75e82202082d38e
putrairawan992 <putrairawan992@yahoo.com>
2020-09-06 12:26:53 +0700

    route/pickup

---
+++ src/pages/Pickup/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pickup(props) {
+    return (
+        <div>
+            <h2>Halaman Pickup</h2>
+        </div>
+    );
+}
+
+export default Pickup;
--- src/routers/routes.js
+++ src/routers/routes.js
@@ -2,6 +2,7 @@ import PATH_URL from "./path";
 import Beranda from "../pages/Beranda";
 import Sidebar from "../layouts/Sidebar";
 import DriverManagement from "../pages/DriverManagement";
+import Pickup from "../pages/Pickup";
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
   {
     path: PATH_URL.DRIVER_MANAGEMENT,
     component: DriverManagement,
+    layout: Sidebar,
+  },
+  {
+    path: PATH_URL.PICKUP,
+    component: Pickup,
     layout: Sidebar,
   },
 ];
